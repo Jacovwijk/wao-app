9c6c8a2e0d3ddfecb32f324175ca952ab1c55acc
Jaco <jaco@studiodrieluik.com>
2016-08-19 14:06:41 +0200

    fileupload + waarderen

--- dataserver.js
+++ dataserver.js
@@ -2,8 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
+var multer = require('multer');
+var upload = multer({dest : './public/uploads/'});
+
 var app = express();
 app.use(bodyParser.json());
+
 
 
 function getConnection() {
@@ -22,12 +26,13 @@ app.listen(3000, function () {
 
 app.use(express.static('public'));
 
-app.post('/files', function (req, res) {
+app.post('/files', upload.any(), function (req, res) {
+  console.log(req.body);
   var connection = getConnection();
   connection.connect();
-  var newFiles = {id: 0, title: req.body.title, category: req.body.category, filetype: req.body.filetype};
+  var newFiles = {id: 0, title: req.body.title, category: req.body.category, filetype: req.body.filetype, image: req.files[0].filename};
   var query = connection.query('INSERT INTO dataluik SET ?', newFiles, function (err, result) {
-    console.log(newFiles);
+    res.send(JSON.stringify(result.insertId));
     res.status(200).end();
   });
   connection.end();
@@ -39,6 +44,7 @@ app.get('/files', function(req, res) {
   connection.query('SELECT * FROM dataluik ORDER BY id DESC', function(err, rows, fields) {
     if (!err) {
       res.send(JSON.stringify(rows));
+      res.status(200).end();
     }
     else {
       console.log('Error while performing Query.');
@@ -54,6 +60,7 @@ app.get('/file/:id', function(req, res){
   connection.query('SELECT * FROM dataluik WHERE id = ?', id, function(err, rows, fields) {
     if (!err) {
       res.send(JSON.stringify(rows));
+      res.status(200).end();
     }
     else {
       console.log('Error while performing Query.');
@@ -69,6 +76,22 @@ app.put('/file/:id', function (req, res) {
   var editFile = {id: idf, title: req.body.title, category: req.body.category};
   var query = connection.query('UPDATE dataluik SET title = ?, category =? WHERE id = ?', [editFile.title, editFile.category, editFile.id] , function (err, result) {
     res.status(200).end();
+  });
+  connection.end();
+});
+
+app.put('/like/:id', function(req, res){
+  var id = req.params.id;
+  var connection = getConnection();
+  connection.connect();
+  connection.query('UPDATE dataluik SET votes = (votes + 1) WHERE id = ?', id, function(err, rows, fields) {
+    if (!err) {
+      console.log('No error found');
+       res.status(200).end();
+    }
+    else {
+      console.log('Error while performing Query.');
+    }
   });
   connection.end();
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,43 @@
 /// <reference path="angular.min.js" />
- angular.module("myApp", ['ngRoute','ui.bootstrap'])
+ angular.module("myApp", ['ngRoute', 'ngCookies', 'ui.bootstrap'])
+
+ 		.directive('fileModel', function ($parse) {
+            return {
+               restrict: 'A',
+               link: function(scope, element, attrs) {
+                  var model = $parse(attrs.fileModel);
+                  var modelSetter = model.assign;
+                  
+                  element.bind('change', function(){
+                     scope.$apply(function(){
+                        modelSetter(scope, element[0].files[0]);
+                     });
+                  });
+               }
+            };
+         })
+      
+        .service('fileUpload', ['$http', '$location', function ($http, $location) {
+            this.uploadFileToUrl = function(file, uploadUrl){
+               var fd = new FormData();
+               for( var key in file)
+               		fd.append(key, file[key]);
+            	console.log(fd);
+               $http.post(uploadUrl, fd, {
+               	  headers: { 'Content-Type' : undefined},
+                  transformRequest: angular.identity
+               })
+            
+               .success(function(data){
+               		var id = data;
+               		$location.path('/file/' + id);
+               })
+            
+               .error(function(){
+               	console.log(fd);
+               });
+            }
+         }])
 
  		.filter('startFrom', function() {
 		    return function(input, start) {
@@ -16,28 +54,21 @@
 				$routeProvider.
 				when('/' , {
 					templateUrl : 'views/home.html',
-					controller : 'myController'
+					controller : 'mainCtrl'
 				}).when('/file/:id', {
 					templateUrl : 'views/file.html',
-					controller : 'myController'
+					controller : 'fileCtrl'
 				}).when('/new-file', {
 					templateUrl : 'views/new-file.html',
-					controller : 'myController'
+					controller : 'newCtrl'
 				}).when('/404', {
-					templateUrl : 'views/404.html',
-					controller : 'myController'
+					templateUrl : 'views/404.html'
 				}).otherwise({ 
 					redirectTo : '/404'
 				});
 		}])
 
-		.controller("myController",  function($scope, $http, $log, $location, $routeParams, $timeout){
-
-				$scope.opslaan = function ()  {
-				    $http.post('/files', angular.toJson($scope.file)).success(function () {
-				    	$location.path('/');
-				    });
-				 };
+		.controller("mainCtrl",  function($scope, $http, $timeout){
 
 				$scope.load = function ()  {
 				    $http.get('/files').
@@ -53,68 +84,10 @@
 				        	console.log(data);
 				     });
 				};
-
 				$scope.load();
 
-
-				$scope.aanpassen = function (id)  {
-				    $http.get("/file/" + id).
-					    success(function(data, status, headers, config) {
-					        $scope.onefile = data[0];
-					      }).
-					    error(function(data, status, headers, config) {
-					        console.log(status);
-					        console.log(data);
-					      });
-				 };
-
-				 $scope.id = $routeParams.id;
-				 $scope.aanpassen($scope.id);
-
-				$scope.edit = function (id)  {
-				    $http.put("/file/" + id, angular.toJson($scope.onefile)).success(function () {
-				    	$scope.aan = false;
-				    });
-				 };
-
-				$scope.verwijderen = function (id)  {
-				    $http.delete("/file/" + id).success(function () {
-				    	$location.path('/');
-				    });
-				 };
-
-				$scope.clear = function ()  {
-				    $scope.onefile = "";
-				 };
-
-				 $scope.aan = false;
-				 $scope.pasaan = function () {
-				 	$scope.aan = true;
-				 }
-				 $scope.cancel = function() {
-				 	$scope.aan = false;
-				 }
-
-				$scope.sortColumn = "title"; 
-				$scope.reverseSort = false; 
 				$scope.catSort = "";
 
-				$scope.sortData = function (column) {
-					$scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
-
-					$scope.sortColumn = column;
-				}
-
-				$scope.getSortClass = function (column) {
-					if ($scope.sortColumn == column) {
-
-						return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
-
-					}
-					return '';
-				}
-
-				// filter op filetype
 				$scope.filterFile = function (file) {
 				 	if($scope.catSort == "") {
 				 		return '' == $scope.catSort ;
@@ -122,7 +95,6 @@
 				 	return file.filetype == $scope.catSort;
 			    }
 
-			    // filter op category
 			    $scope.filter = {};
 
 			    $scope.getCategories = function () {
@@ -133,19 +105,10 @@
 			        });
 			    };
 
-			     $scope.getCategories = function () {
-			        return ($scope.files || []).map(function (w) {
-			            return w.category;
-			        }).filter(function (w, idx, arr) {
-			            return arr.indexOf(w) === idx;
-			        });
-			    };
-
-
-
 			    $scope.filterByCategory = function (file) {
 			        return $scope.filter[file.category] || noFilter($scope.filter);
 			    };
+			    
 			    function noFilter(filterObj) {
 			        for (var key in filterObj) {
 			            if (filterObj[key]) {
@@ -164,6 +127,99 @@
 			    $scope.setPage = function(pageNo) {
        			 	$scope.currentPage = pageNo;
     			};
-
-		});
+		})
 		
+		.controller("fileCtrl",  function($scope, $cookies, $http, $location, $routeParams, $timeout){
+
+				$scope.load = function ()  {
+				    $http.get('/files').
+				     	success(function(data, status, headers, config) {
+				        	$scope.files = data;
+				      }).
+				     	error(function(data, status, headers, config) {
+				       		console.log(status);
+				        	console.log(data);
+				     });
+				};
+				$scope.load();
+
+				$scope.aanpassen = function (id)  {
+				    $http.get("/file/" + id).
+					    success(function(data, status, headers, config) {
+					        $scope.onefile = data[0];
+					      }).
+					    error(function(data, status, headers, config) {
+					        console.log(status);
+					        console.log(data);
+					      });
+				};
+				$scope.id = $routeParams.id;
+				$scope.aanpassen($scope.id);
+
+				$scope.edit = function (id)  {
+				    $http.put("/file/" + id, angular.toJson($scope.onefile)).success(function () {
+				    	$scope.aan = false;
+				    });
+				};
+
+				$scope.verwijderen = function (id)  {
+				    $http.delete("/file/" + id).success(function () {
+				    	$location.path('/');
+				    });
+				};
+
+				$scope.clear = function ()  {
+				    $scope.onefile = "";
+				};
+
+				$scope.aan = false;
+				$scope.pasaan = function () {
+				 	$scope.aan = true;
+				}
+
+				$scope.cancel = function() {
+				 	$scope.aan = false;
+				}
+
+				$scope.waardeer = function(id) {
+					if($cookies.get("Cookie" + id) == id) {
+						$scope.showGreeting = true;
+				       	$timeout(function(){
+				          $scope.showGreeting = false;
+				       	}, 1500);
+					} else {
+						$http.put('/like/' + id).
+							success(function() {
+								console.log('liked');
+								$scope.onefile.votes ++;
+							})
+							.error(function() {
+
+							});
+							$cookies.put("Cookie" + id, id); 
+					}
+				}
+		})
+
+		.controller("newCtrl",  function($scope, $http, $location, fileUpload){
+				$scope.file = {};
+				$scope.opslaan = function(){
+	               var file = $scope.file;	                             
+	               var uploadUrl = "/files";
+
+	               fileUpload.uploadFileToUrl(file, uploadUrl);
+	            };
+
+
+				$scope.load = function ()  {
+				    $http.get('/files').
+				     	success(function(data, status, headers, config) {
+				        	$scope.files = data;
+				      }).
+				     	error(function(data, status, headers, config) {
+				       		console.log(status);
+				        	console.log(data);
+				     });
+				};
+				$scope.load();
+		});
